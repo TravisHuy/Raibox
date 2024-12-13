2b2ed0329104872edcbd7d13f244ae8266b1e5c3
TravisHuy <honhathuy098@gmail.com>
2024-12-13 21:08:21 +0700

    init

---
+++ src/main/java/org/travishuy/raibox/RaiboxApplication.java
@@ -0,0 +1,13 @@
+package org.travishuy.raibox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaiboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/travishuy/raibox/RaiboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.travishuy.raibox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
